e0ec07e52a78f902165790706ded4010c784d1cc
Avnish <com.avnish@gmail.com>
2025-10-13 00:15:39 +0530

    Config. Updated

--- etawah/etawah/settings.py
+++ etawah/etawah/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-m-b=4f7d%gqt8x&^f6a=%n92g_h4vw%7w_2+05h@ard1irrzap'
-
 DEBUG = False
 
 ALLOWED_HOSTS = [
@@ -14,7 +13,6 @@ ALLOWED_HOSTS = [
     '135.235.228.118',
     'cosmic-centralindia-ns-4187579137fc.trafficmanager.net',
 ]
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -46,7 +44,6 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -64,13 +61,6 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
@@ -79,9 +69,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'shop' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#APPEND_SLASH = False
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
